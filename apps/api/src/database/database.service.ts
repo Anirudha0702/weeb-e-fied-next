93991c089b4298dbf570d49d99133c07b43a675d
ANIRUDHA PRADHAN <114860976+Anirudha0702@users.noreply.github.com>
2025-05-24 23:45:44 +0530

    feat(database): implement connection and disconnection lifecycle methods

--- apps/api/src/database/database.service.ts
+++ apps/api/src/database/database.service.ts
@@ -2,7 +2,18 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'info', 'warn', 'error']
+          : ['error'],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
