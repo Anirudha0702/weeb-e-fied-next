56a08971e8c20e50eb336a53bea47a5aec8f7ef9
Anirudha0702 <anirudhapradhan403@gmail.com>
2026-02-23 13:02:59 +0530

    fix: revert to using config.get for database connection parameters in TypeOrm configuration

--- apps/api/src/config/orm.config.ts
+++ apps/api/src/config/orm.config.ts
@@ -5,15 +5,15 @@ export const getTypeOrmConfig = (
   config: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: config.getOrThrow('DB_HOST'),
-  port: parseInt(config.getOrThrow('DB_PORT') ?? '5432', 10),
-  username: config.getOrThrow('DB_USER'),
-  password: config.getOrThrow('DB_PASSWORD'),
-  database: config.getOrThrow('DB_NAME'),
+  host: config.get('DB_HOST'),
+  port: parseInt(config.get('DB_PORT') ?? '5432', 10),
+  username: config.get('DB_USER'),
+  password: config.get('DB_PASSWORD'),
+  database: config.get('DB_NAME'),
   entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
   logging: true,
   ssl: {
-    ca: config.getOrThrow('DB_CA'),
+    ca: config.get('DB_CA'),
     rejectUnauthorized: true,
   },
   synchronize: true, // Recommended to control sync via env
